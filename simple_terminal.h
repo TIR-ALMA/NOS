ee617537cfa80e1657b276aa336cc179ad0b2c09
TIR-ALMA <visotskayoa@mail.ru>
2026-03-20 21:33:51 +0700

    Create simple_terminal.h

---
+++ simple_terminal.h
@@ -0,0 +1,12 @@
+// simple_terminal.h
+#ifndef SIMPLE_TERMINAL_H
+#define SIMPLE_TERMINAL_H
+
+// Объявление основной функции терминала
+void simple_terminal_run();
+
+// Объявление вашей версии обработчика клавиатуры
+void simple_kb_handler();
+
+#endif // SIMPLE_TERMINAL_H
+
